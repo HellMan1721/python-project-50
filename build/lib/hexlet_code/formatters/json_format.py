fe4bae332b31c1e7d5bcc43b1d41d753824c68ad
HellMan1721 <highwayhellman@gmail.com>
2025-11-21 10:48:33 +0300

    update

--- build/lib/hexlet_code/formatters/json_format.py
+++
@@ -1,6 +0,0 @@
-import json
-
-
-def json_format(diff_tree):
-    """Format diff as JSON string"""
-    return json.dumps(diff_tree, indent=2)
--- venv/Lib/site-packages/hexlet_code/formatters/__init__.py
+++
@@ -1,5 +0,0 @@
-from .json_format import json_format
-from .plain import plain
-from .stylish import format_diff
-
-__all__ = ['json_format', 'plain', 'format_diff']
