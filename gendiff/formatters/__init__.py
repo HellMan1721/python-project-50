0671d99ef9f4e0c0620a4e08135aabf29d1e7d9e
HellMan1721 <highwayhellman@gmail.com>
2025-11-29 20:37:28 +0300

    update

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,5 +1,5 @@
-from gendiff.formatters import json_format
-from gendiff.formatters import plain
-from gendiff.formatters import stylish
+from .json_format import json_format
+from .plain import plain
+from .stylish import stylish
 
 __all__ = ['json_format', 'plain', 'stylish']
